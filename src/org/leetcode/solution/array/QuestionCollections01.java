23b5ea7271aa91452152ae1a4f2f8d8de41be7f5
BladeMasterKing <wangbodang2@126.com>
2023-05-26 14:56:11 +0800

    leetcode0001

---
+++ src/org/leetcode/solution/array/QuestionCollections01.java
@@ -0,0 +1,44 @@
+package org.leetcode.solution.array;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class QuestionCollections01 {
+
+	/**
+	 * 题目：0001
+	 * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那两个整数，
+	 * 并返回它们的数组下标。你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+	 * 输入：nums = [2,7,11,15], target = 9
+	 * 输出：[0,1]
+	 * 解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+	 */
+	
+	/**
+	 * 暴力枚举
+	 */
+	public int[] q0001Soluiton1(int[] nums, int target) {
+		for(int i=0;i<nums.length;++i) {
+			for(int j=i+1;j<nums.length;++j) {
+				if(i != j && target == nums[i]+nums[j]) {
+					return new int[] {i,j};
+				}
+			}
+		}
+		return new int[0];
+	}
+	/**
+	 * 哈希表
+	 */
+	public int[] q0001Solution2(int[] nums, int target) {
+		Map<Integer,Integer> hashTable = new HashMap<Integer,Integer>();
+		for(int i=0;i<nums.length;++i) {
+			if(hashTable.containsKey(target - nums[i])){
+				return new int[] {i, hashTable.get(target - nums[i]), i};
+			}
+			hashTable.put(nums[i], i);
+		}
+		return new int[0];
+	}
+	
+}
